1037b7c79a50e6b96e16bac48b5f19b2dd58a752
Ralph Perdomo <ralph@prdmo.com>
2019-02-11 20:26:10 -0800

    update webpack title

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:'template.ejs',
       appMountId: 'react-app-root',
-      title: 'React Help Queue',
+      title: 'React Template Environment',
       filename: resolve(__dirname, "build", "index.html"),
     })
   ],
