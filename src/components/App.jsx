c8cc801055834a9bce07fc1c5983dd699f254031
Ralph Perdomo <ralph@prdmo.com>
2019-02-11 21:24:32 -0800

    add styled-jsx module

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello, world</h1>
+      <h1>Hello, world</h1>   
+
+      <style jsx global>{`
+          body{
+            font-family: sans-serif;
+          }
+        `}</style>
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: [ "react-hot-loader/babel" ]
+            plugins: [ 'react-hot-loader/babel', 'styled-jsx/babel' ]
           }
         }
       }
