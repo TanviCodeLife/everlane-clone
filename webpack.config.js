b44be00846b2d354f35e02de4c892dafb2298fe4
Ralph Perdomo <ralph@prdmo.com>
2019-02-11 20:17:14 -0800

    add eslint to webpack env

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,17 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
+        enforce: "pre",
+        loader: "eslint-loader",
+        exclude: /node_modules/,
+        options: {
+          emitWarning: true,
+          configFile: "./.eslintrc.json"
+          }
+        },
+        
+      {
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
