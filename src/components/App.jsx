b2669ba5db1c86d22761bdd945224d4f1c5e2aaa
Ralph Perdomo <ralph@prdmo.com>
2019-02-12 22:43:53 -0800

    add react-router-dom to env

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <h1>Hello, world</h1>   
+      <h1>Hello, world</h1>
+
+      <Switch>
+        <Route exact path='/' component={ ( ()=>{'Routing works!';} )() } /> 
+      </Switch>
 
       <style jsx global>{`
           body{
